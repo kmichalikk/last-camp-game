a293605eb622f261198c72e6d13cc1f22868b8fd
Krzysztof Ligarski <ligarski.k@gmail.com>
2026-03-14 10:23:18 +0100

    highlight selection

--- player.gd
+++ player.gd
@@ -6,17 +6,22 @@ class_name Player
 
 @onready var geometry: GeometryInstance3D = $PlayerCSG
 
+@onready var material: StandardMaterial3D = geometry.material_override
+
 func _ready() -> void:
-	var material = StandardMaterial3D.new()
 	material.albedo_color = player_color
-	geometry.material_override = material
-	
+	material.emission = player_color
+	material.emission_enabled = false
 
 func _input_event(_camera: Camera3D, any_input_event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if any_input_event is InputEventMouseButton and any_input_event.is_pressed():
 		if GameState.target_player != self:
+			if GameState.target_player != null:
+				GameState.target_player.material.emission_enabled = false
 			GameState.target_player = self
+			material.emission_enabled = true
 			print("Player selected")
 		else:
 			GameState.target_player = null
+			material.emission_enabled = false
 			print("Player deselected")
