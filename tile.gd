4112c87429bbc8425ab042b9514dc76a3eec747a
Krzysztof Ligarski <ligarski.k@gmail.com>
2026-03-14 11:33:02 +0100

    prevent unnecessary color overrides

--- tile.gd
+++ tile.gd
@@ -9,13 +9,14 @@ class_name RockTile
 
 @export var player_can_stand_on: bool = true
 
+func _ready() -> void:
+	GameState.selection_changed.connect(_on_selection_changed)
+
 func has_standing_player() -> bool:
 	return topOccupancyZone.has_overlapping_bodies()
 
 func _mouse_enter() -> void:
 	if GameState.target_player != null and GameState.can_player_move_to_tile(GameState.target_player, self):
-		var target_color = GameState.target_player.player_color
-		material.albedo_color = Color(target_color.r, target_color.g, target_color.b, material.albedo_color.a)
 		tile_stand_highlight.visible = true
 
 
@@ -27,3 +28,8 @@ func _input_event(_camera: Camera3D, any_input_event: InputEvent, _position: Vec
 	if any_input_event is InputEventMouse:
 		if any_input_event is InputEventMouseButton and GameState.target_player and GameState.can_player_move_to_tile(GameState.target_player, self):
 			GameState.target_player.position = self.global_position + Vector3(0, 1, 0)
+			
+func _on_selection_changed(new_player: Player):
+	if new_player != null:
+		var target_color = GameState.target_player.player_color
+		material.albedo_color = Color(target_color.r, target_color.g, target_color.b, material.albedo_color.a)
