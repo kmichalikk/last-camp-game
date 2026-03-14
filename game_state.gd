11c52b874c4f3c2f1761fcb51c8d96e35ae01e2d
Krzysztof Ligarski <ligarski.k@gmail.com>
2026-03-14 10:33:46 +0100

    move selection logic to game state

--- game_state.gd
+++ game_state.gd
@@ -1,3 +1,23 @@
 extends Node
 
+signal selection_changed(new_player: Player)
+
 var target_player: Player = null
+
+func toggle_selection(player: Player):
+	if target_player == player:
+		_deselect_player()
+	else:
+		_select_player(player)
+
+func _select_player(player: Player):
+	if target_player == player: return
+	
+	target_player = player
+	selection_changed.emit(target_player)
+
+func _deselect_player():
+	if target_player == null: return
+	
+	target_player = null
+	selection_changed.emit(null)
--- player.gd
+++ player.gd
@@ -4,24 +4,18 @@ class_name Player
 
 @export var player_color: Color = "498eff"
 
-@onready var geometry: GeometryInstance3D = $PlayerCSG
-
-@onready var material: StandardMaterial3D = geometry.material_override
+@onready var material: StandardMaterial3D = $PlayerCSG.material_override
 
 func _ready() -> void:
 	material.albedo_color = player_color
 	material.emission = player_color
 	material.emission_enabled = false
+	
+	GameState.selection_changed.connect(_on_selection_changed)
 
 func _input_event(_camera: Camera3D, any_input_event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if any_input_event is InputEventMouseButton and any_input_event.is_pressed():
-		if GameState.target_player != self:
-			if GameState.target_player != null:
-				GameState.target_player.material.emission_enabled = false
-			GameState.target_player = self
-			material.emission_enabled = true
-			print("Player selected")
-		else:
-			GameState.target_player = null
-			material.emission_enabled = false
-			print("Player deselected")
+		GameState.toggle_selection(self)
+
+func _on_selection_changed(new_player: Player):
+	material.emission_enabled = new_player == self
