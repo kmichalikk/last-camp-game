1b0ccbecc55b21945dc4874a708c44702ef482c9
Krzysztof Ligarski <ligarski.k@gmail.com>
2026-03-14 10:16:41 +0100

    different colors for players (visible in runtime)

--- player.gd
+++ player.gd
@@ -2,6 +2,16 @@ extends AnimatableBody3D
 
 class_name Player
 
+@export var player_color: Color = "498eff"
+
+@onready var geometry: GeometryInstance3D = $PlayerCSG
+
+func _ready() -> void:
+	var material = StandardMaterial3D.new()
+	material.albedo_color = player_color
+	geometry.material_override = material
+	
+
 func _input_event(_camera: Camera3D, any_input_event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if any_input_event is InputEventMouseButton and any_input_event.is_pressed():
 		if GameState.target_player != self:
