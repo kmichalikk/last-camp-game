ce9f2ecaa76adafb69728789175e50bdc30907cc
Krzysztof Ligarski <63918941+kligarski@users.noreply.github.com>
2026-03-14 11:39:03 +0100

    feat: add multiple players and handle colors (#3)
    * group rocks
    
    * add 2 players
    
    * different colors for players (visible in runtime)
    
    * highlight selection
    
    * move selection logic to game state
    
    * make tile selection match player color
    
    * prevent overlap
    
    * prevent unnecessary color overrides
    
    * formatting

--- game_state.gd
+++ game_state.gd
@@ -1,3 +1,34 @@
 extends Node
 
+signal selection_changed(new_player: Player)
+
 var target_player: Player = null
+
+func toggle_selection(player: Player):
+	if target_player == player:
+		_deselect_player()
+	else:
+		_select_player(player)
+
+func _select_player(player: Player):
+	if target_player == player: return
+	
+	target_player = player
+	selection_changed.emit(target_player)
+
+func _deselect_player():
+	if target_player == null: return
+	
+	target_player = null
+	selection_changed.emit(null)
+
+func can_player_move_to_tile(player: Player, tile: RockTile):
+	if not tile.player_can_stand_on:
+		return false
+
+	if tile.has_standing_player():
+		return false
+		
+	# Additional constraints in the future will go here.
+	
+	return true
--- player.gd
+++ player.gd
@@ -2,11 +2,20 @@ extends AnimatableBody3D
 
 class_name Player
 
+@onready var material: StandardMaterial3D = $PlayerCSG.material_override
+
+@export var player_color: Color = "498eff"
+
+func _ready() -> void:
+	material.albedo_color = player_color
+	material.emission = player_color
+	material.emission_enabled = false
+	
+	GameState.selection_changed.connect(_on_selection_changed)
+
 func _input_event(_camera: Camera3D, any_input_event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if any_input_event is InputEventMouseButton and any_input_event.is_pressed():
-		if GameState.target_player != self:
-			GameState.target_player = self
-			print("Player selected")
-		else:
-			GameState.target_player = null
-			print("Player deselected")
+		GameState.toggle_selection(self)
+
+func _on_selection_changed(new_player: Player):
+	material.emission_enabled = new_player == self
